38fcf3795479132e4f387153e37c3de9a3f12c03
XsiSec <xsisec@outlook.com>
2019-12-17 11:29:30 +0100

    added greenblack theme

---
+++ [Openbox] Green Hacker/conky/conky-spotify/scripts/cover.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+id_current=$(cat /home/xsisec/.config/conky/conky-spotify/current/current.txt)
+id_new=`/home/xsisec/.config/conky/conky-spotify/scripts/id.sh`
+cover=
+imgurl=
+
+if [ "$id_new" != "$id_current" ]; then
+
+	cover=`ls /home/xsisec/.config/conky/conky-spotify/covers/ | grep $id_new`
+
+	if [ "$cover" == "" ]; then
+
+	    imgurl=`/home/xsisec/.config/conky/conky-spotify/scripts/imgurl.sh $id_new`
+	    wget -q -O /home/xsisec/.config/conky/conky-spotify/covers/$id_new.jpg $imgurl &> /dev/null
+		# clean up covers folder, keeping only the latest X amount, in below example it is 10
+	    rm -f `ls -t /home/xsisec/.config/conky/conky-spotify/covers/* | awk 'NR>10'`
+	    #rm wget-log #wget-logs are accumulated otherwise
+	    cover=`ls /home/xsisec/.config/conky/conky-spotify/covers | grep $id_new`
+	fi
+
+	if [ "$cover" != "" ]; then
+		cp /home/xsisec/.config/conky/conky-spotify/covers/$cover /home/xsisec/.config/conky/conky-spotify/current/current.jpg
+	else
+		cp /home/xsisec/.config/conky/conky-spotify/empty.jpg /home/xsisec/.config/conky/conky-spotify/current/current.jpg
+	fi
+
+	echo $id_new > /home/xsisec/.config/conky/conky-spotify/current/current.txt
+fi
