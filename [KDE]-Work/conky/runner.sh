67f04deb1886f5830a618ff23d9a7aa60cbd6e6b
XsiSec <xsisec@outlook.com>
2023-06-09 13:34:58 +0200

    added work

---
+++ [KDE]-Work/conky/runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+conky -c ~/.config/conky/.conkyrc_clock &
+conky -c ~/conky_weatherbox/conky_weatherbox &
+conky -c ~/.config/conky/.conkyrc_sys &
+conky -c ~/.config/conky/.conkyrc_net &
+conky -c ~/.config/conky/.conkyrc_disk &
+conky -c ~/.config/conky/conky-news/.conky_news_1 &
+conky -c ~/.config/conky/conky-news/.conky_news_2 &
+conky -c ~/.config/conky/conky-news/.conky_news_3 &
+conky -c ~/.config/conky/conky-news/.conky_news_4 &
+conky -c ~/.config/conky/conky-news/.conky_news_5 &
+conky -c ~/.config/conky/conky-news/.conky_news_6 &
+conky -c ~/.config/conky/.conkyrc_spotify
